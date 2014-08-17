a91cfca80f32e6473cabcf9abd45647d74d816f9
Kyle Diedrick <corporealshift@gmail.com>
2014-08-16 19:38:46 -0700

    Move everything into template specific folder

--- www/top-nav/config.rb
+++ www/top-nav/config.rb
@@ -2,10 +2,10 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "www/css"
-sass_dir = "www/scss"
-images_dir = "www/img"
-javascripts_dir = "www/js"
+css_dir = "css"
+sass_dir = "scss"
+images_dir = "img"
+javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
